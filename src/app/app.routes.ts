812ca0b81e0566187778e87cbbae93e63658c3ab
12131555 <12131555@me.mcu.edu.tw>
2024-01-11 05:23:50 +0800

    feat(article-form): 文章表單頁面實作新增

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,12 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
 import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
 import { ArticleFormComponent } from './article-form/article-form.component';
+import { ArticleFormPageComponent } from './article-form-page/article-form-page.component';
 
 export const routes: Routes = [
     { path: ' ', pathMatch: 'full', redirectTo: 'home' },
     { path: 'home', component: HomePageComponent },
     { path: 'sign-in', component: SignInPageComponent },
     { path: 'sign-up', component: SignUpPageComponent },
-    { path: 'article/form', component: ArticleFormComponent },
+    { path: 'article/form', component: ArticleFormPageComponent },
 ];
--- src/app/article-form-page/article-form-page.component.ts
+++ src/app/article-form-page/article-form-page.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-article-form-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './article-form-page.component.html',
   styleUrl: './article-form-page.component.css'
 })
