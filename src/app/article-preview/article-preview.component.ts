eca9cd9a4abecb58044b0c7750fcd13654ab7ce6
12131555 <12131555@me.mcu.edu.tw>
2024-01-11 06:14:02 +0800

    feat(model): 文章資料類別定義新增

--- src/app/article-preview/article-preview.component.ts
+++ src/app/article-preview/article-preview.component.ts
@@ -1,17 +1,34 @@
-import { CommonModule, NgFor } from '@angular/common';
+import { CommonModule, DatePipe, NgFor } from '@angular/common';
 import { Component, HostBinding } from '@angular/core';
 import { TagListComponent } from '../tag-list/tag-list.component';
 import { TagComponent } from '../tag/tag.component';
 import { FavoriteButtonComponent } from '../favorite-button/favorite-button.component';
+import { Article } from '../article';
 
 @Component({
   selector: 'app-article-preview',
   standalone: true,
-  imports: [FavoriteButtonComponent, TagListComponent],
+  imports: [DatePipe, FavoriteButtonComponent, TagListComponent],
   templateUrl: './article-preview.component.html',
   styleUrl: './article-preview.component.css'
 })
 export class ArticlePreviewComponent {
-  @HostBinding('class') class = 'article-preview' 
-  tags = ['enim', 'repellat', 'est', 'eos'];
+  @HostBinding('class') class = 'article-preview' ;
+  
+    tags = ['enim', 'repellat', 'est', 'eos'];
+    article =new Article({
+      id:1 ,
+      title:
+      'Ill quantify the redundant TCP bus, that should hard drive the ADP bandwidth!',
+    content: `
+    Aut facilis qui. Cupiditate sit ratione eum sunt rerum impedit. Qui suscipit 
+     debitis et et voluptates voiuptatem voluptatibus. Quas voluptatum quae
+     corporis corporis possimus.
+     `,
+      favoriteCount: 30,
+      author: 'Oliver',
+      createDate: new Date(2024, 6 ,4),
+      tags: ['enim', 'repellat', 'est', 'eos'],
+    });
 }
+ 
---
+++ src/app/article.ts
@@ -0,0 +1,20 @@
+
+export class Article {
+    constructor(initData?: Partial<Article>){
+        Object.assign(this, initData);
+    }
+
+    id!: number;
+
+    title!: string;
+
+    content!: string;
+
+    favoriteCount!: number;
+
+    author!: string;
+
+    createDate!: Date;
+    
+    tags!: string[];
+}
